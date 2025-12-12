e26254b7ecb7c6213588ca2e2b24455c7a46d72a
Avilash Roy <76527137+AviPopo@users.noreply.github.com>
2025-12-12 13:35:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import app from './src/app.js';
 import Bun from 'bun';
 
 const bunApp = Bun.serve({
-  port: 3030,
+  // Use Render's assigned port OR fallback to 3030 for localhost
+  port: process.env.PORT || 3030, 
+  hostname: "0.0.0.0", // Ensure it binds to all interfaces, required for Render
   fetch: app.fetch,
 });
 
